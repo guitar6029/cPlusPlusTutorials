cb6c0968bfe9cc8c0d4956a25fac4fef597e3682
Josh <jsbusiness305@gmail.com>
2025-02-16 20:13:31 -0500

    added lesson9

---
+++ lesson9.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int i = 0;
+    while (i < 10){
+        if (i == 4){
+           break;
+        }
+        cout << i << "\n";
+    
+        i++;
+    }
+
+    string cars[5] = {"Volvo", "BMW", "Ford", "Mazda", "Tesla"};
+
+    int cars_length = sizeof(cars) / sizeof(cars[0]);
+
+    for (int i = 0; i < cars_length; i++){
+        cout << cars[i] << "\n";
+    }
+}
